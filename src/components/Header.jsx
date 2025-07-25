21699b0eb9a15f117f299b4e8add4052f8bb0ca4
noman shaber <nimrashaheen451@gmail.com>
2025-07-25 09:53:18 +0500

    setup Project today

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <nav>
+            <h1>E-commerce Store</h1>\
+            <div>
+                
+            </div>
+        </nav>
+    </header>
+    
+    
+    </>
+  )
+}
+
+export default Header
